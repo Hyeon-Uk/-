227c22452329d5f8b1387ea899a0c3af810165f6
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-03-15 15:22:44 +0900

    [MemberAuthService] Login Test Complete

--- src/test/java/com/hyeonuk/chatting/member/service/MemberAuthServiceImplTest.java
+++ src/test/java/com/hyeonuk/chatting/member/service/MemberAuthServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.hyeonuk.chatting.member.service;
 
 import com.hyeonuk.chatting.member.dto.JoinDto;
+import com.hyeonuk.chatting.member.dto.LoginDto;
 import com.hyeonuk.chatting.member.dto.MemberDto;
 import com.hyeonuk.chatting.member.entity.Member;
 import com.hyeonuk.chatting.member.exception.AlreadyExistException;
+import com.hyeonuk.chatting.member.exception.NotFoundException;
 import com.hyeonuk.chatting.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,35 +31,35 @@ class MemberAuthServiceImplTest {
     @Mock
     private MemberRepository mockMemberRepository;
 
-    JoinDto joinDto1,joinDto2,joinDto3;
-
-    @BeforeEach
-    public void init(){
-        joinDto1 = JoinDto.builder()
-                .email("user1@gmail.com")
-                .password("user1Password")
-                .passwordCheck("user1Password")
-                .nickname("user1")
-                .build();
-
-        joinDto2 = JoinDto.builder()
-                .email("user2@gmail.com")
-                .password("user2Password")
-                .passwordCheck("user2Password")
-                .nickname("user2")
-                .build();
-
-        joinDto3 = JoinDto.builder()
-                .email("user3@gmail.com")
-                .password("user3Password")
-                .passwordCheck("user3Password")
-                .nickname("user3")
-                .build();
-    }
 
     @Nested
     @DisplayName("save test")
     public class SaveTest{
+        JoinDto joinDto1,joinDto2,joinDto3;
+
+        @BeforeEach
+        public void init(){
+            joinDto1 = JoinDto.builder()
+                    .email("user1@gmail.com")
+                    .password("user1Password")
+                    .passwordCheck("user1Password")
+                    .nickname("user1")
+                    .build();
+
+            joinDto2 = JoinDto.builder()
+                    .email("user2@gmail.com")
+                    .password("user2Password")
+                    .passwordCheck("user2Password")
+                    .nickname("user2")
+                    .build();
+
+            joinDto3 = JoinDto.builder()
+                    .email("user3@gmail.com")
+                    .password("user3Password")
+                    .passwordCheck("user3Password")
+                    .nickname("user3")
+                    .build();
+        }
         @Nested
         @DisplayName("save test success")
         public class Success{
@@ -134,6 +136,76 @@ class MemberAuthServiceImplTest {
 
                 sb.append(joinDto1.getNickname()).append("은 이미 존재하는 닉네임입니다.");
                 assertThat(message).isEqualTo(sb.toString());
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("login test")
+    public class LoginTest{
+        LoginDto loginDto;
+        Member loginMember;
+
+        @BeforeEach
+        public void init(){
+            loginDto = LoginDto.builder()
+                    .email("test@gmail.com")
+                    .password("test")
+                    .build();
+            loginMember = Member.builder()
+                    .id(1l)
+                    .email(loginDto.getEmail())
+                    .nickname("test")
+                    .password(loginDto.getPassword())
+                    .build();
+        }
+
+        @Nested
+        @DisplayName("login test success")
+        public class Success{
+            @Test
+            public void successTest(){
+                //given
+                when(mockMemberRepository.findByEmail(loginDto.getEmail())).thenReturn(Optional.ofNullable(loginMember));
+
+                //when
+                MemberDto login = memberAuthService.login(loginDto);
+
+                //then
+                assertThat(login.getNickname()).isEqualTo(loginMember.getNickname());
+                assertThat(login.getEmail()).isEqualTo(loginMember.getEmail());
+                assertThat(login.getId()).isEqualTo(loginMember.getId());
+
+                verify(mockMemberRepository,times(1)).findByEmail(loginDto.getEmail());
+            }
+        }
+
+        @Nested
+        @DisplayName("login test failure")
+        public class Failure{
+            @Test
+            @DisplayName("password not match Exception")
+            public void passwordNotMatchExceptionTest(){
+                //given
+                when(mockMemberRepository.findByEmail(any())).thenReturn(
+                        Optional.ofNullable(Member.builder()
+                                .password(loginMember.getPassword().concat("not matches"))
+                                .build())
+                );
+
+                //when & then
+                String message = assertThrows(IllegalArgumentException.class, () -> memberAuthService.login(loginDto)).getMessage();
+                assertThat(message).isEqualTo("비밀번호가 일치하지 않습니다.");
+            }
+            @Test
+            @DisplayName("email not found Exception")
+            public void emailNotFoundExceptionTest(){
+                //given
+                when(mockMemberRepository.findByEmail(any())).thenReturn(Optional.ofNullable(null));
+
+                //when & then
+                String message = assertThrows(NotFoundException.class,()->memberAuthService.login(loginDto)).getMessage();
+                assertThat(message).isEqualTo("해당하는 유저가 존재하지 않습니다.");
             }
         }
     }
