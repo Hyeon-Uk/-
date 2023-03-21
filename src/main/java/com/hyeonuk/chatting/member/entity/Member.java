6b8623e081fce0dc378673c7d847b37a04400378
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-03-21 09:19:23 +0900

    Member에 친구 맵핑

--- src/main/java/com/hyeonuk/chatting/member/entity/Member.java
+++ src/main/java/com/hyeonuk/chatting/member/entity/Member.java
@@ -2,10 +2,7 @@ package com.hyeonuk.chatting.member.entity;
 
 import com.hyeonuk.chatting.integ.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.util.ArrayList;
@@ -30,6 +27,9 @@ public class Member extends BaseEntity {
     @Column(name= "nickname",unique = true,nullable = false)
     private String nickname;
 
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL,orphanRemoval = true)
-    private List<Friendship> friendships = new ArrayList<>();
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "friendship",
+            joinColumns = @JoinColumn(name="member_id"),
+            inverseJoinColumns = @JoinColumn(name="friend_id"))
+    private List<Member> friends = new ArrayList<>();
 }
